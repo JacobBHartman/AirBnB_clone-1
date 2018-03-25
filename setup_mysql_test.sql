2282a7d67bafd33a5f9417101f58defdf9efaf56
jacobbhartman <209@holbertonschool.com>
2018-03-25 20:23:03 +0000

    dont need to identify by pwd

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
 -- this MySQL script prepares a MySQL server for 
 -- the AirBnB clone
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
