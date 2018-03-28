49ccbda10990aa83d9622518116a8b6ad530aa7f
Jacob Biehler <biehlerj@gmail.com>
2018-03-27 17:56:50 -0700

    Updating multiple classes (tasks 7-9)

--- models/city.py
+++ models/city.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-'''
+"""
     Define the class City.
-'''
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    '''
+    """
         Define the class City that inherits from BaseModel.
-    '''
+    """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade='delete')
