c65a7eac535fa347e1779d9a18dfbf29934f774a
jacobbhartman <209@holbertonschool.com>
2018-03-28 00:48:37 +0000

    now properly deleting _sa_instance

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+#            models.storage.new(self)
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
@@ -68,9 +68,8 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        str_del = "_sa_instance_state"
-        if str_del in cp_dct:
-            del cp_dct[str_del]
+        if '_sa_instance_state' in cp_dct:
+            del cp_dct['_sa_instance_state']
         return (cp_dct)
 
     def delete(self):
